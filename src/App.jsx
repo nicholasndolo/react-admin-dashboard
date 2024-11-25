d71bd7c0366b5119dacc55e094c1810d5955a9e9
Nicholas <nickndolo95@gmail.com>
2024-11-25 10:34:12 +0300

    feat: implement animated sidebar with navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import { Route, Routes } from 'react-router-dom'
+import OverviewPage from './pages/OverviewPage';
+import ProductsPage from './pages/ProductsPage';
+
+import Sidebar from './components/Sidebar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <div className='flex bg-gray-900 text-gray-100 overflow-hidden'>
+  <div className='flex bg-gray-900 h-screen text-gray-100 overflow-hidden'>
+    <div className='fixed inset-0 z-0'>
+      <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80'/>
+      <div className='absolute inset-0 backdrop-blur-sm'/>
+    </div>
+    <Sidebar/>
     <Routes>
-      <Route path='/' element={<OverviewPage/>}/>
+      <Route path='/' element={<OverviewPage />}/>
       <Route path='/products' element={<ProductsPage/>}/>
     </Routes>
   </div>
