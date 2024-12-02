b3ac9459403f81f79b1b65129426d407dd2a8128
Nicholas <nickndolo95@gmail.com>
2024-12-02 16:22:19 +0300

    feat: RevenueChart component

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/common/Header';
 import OverviewCards from '../components/analytics/OverviewCards';
+import RevenueChart from '../components/analytics/RevenueChart';
 
 const AnalyticsPage = () => {
   return (
@@ -8,6 +9,7 @@ const AnalyticsPage = () => {
       <Header title='Analytics Dashboard' />
       <main className='max-w-7xl mx-auto px-4 lg:px-8 py-6'>
         <OverviewCards />
+        <RevenueChart />
       </main>
     </div>
   )
