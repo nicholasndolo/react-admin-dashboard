873856003b3ce01412dc2838cc8d731bb8051a8e
Nicholas <nickndolo95@gmail.com>
2024-12-02 18:17:01 +0300

    feat: ChannelPerformance component

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import Header from '../components/common/Header';
+import Header from '../components/common/Header'
+
 import OverviewCards from '../components/analytics/OverviewCards';
 import RevenueChart from '../components/analytics/RevenueChart';
+import ChannelPerformance from '../components/analytics/ChannelPerformance';
+import ProductPerformance from '../components/analytics/ProductPerformance';
+import UserRetention from '../components/analytics/UserRetention';
+import CustomerSegmentation from '../components/analytics/CustomerSegmentation';
 
 const AnalyticsPage = () => {
   return (
@@ -10,6 +14,13 @@ const AnalyticsPage = () => {
       <main className='max-w-7xl mx-auto px-4 lg:px-8 py-6'>
         <OverviewCards />
         <RevenueChart />
+
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8'>
+          <ChannelPerformance />
+          <ProductPerformance />
+          <UserRetention />
+          <CustomerSegmentation />
+        </div>
       </main>
     </div>
   )
